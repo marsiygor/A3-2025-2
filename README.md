90fe7944d03e9ebe29a210c73d53354f81e41d71
marsiygor <ygor.marsi@gmail.com>
2025-10-27 22:47:11 -0300

    Update .gitignore and enhance admin model registration with custom user fields

---
+++ README.md
@@ -0,0 +1,6 @@
+Lucas Santos Carvalho - 82211118
+Joao Vitor Nonato - 822163044
+Ygor Fernandes Marsi - 822156379
+Ana Carolina Amaral Silva - 822138023
+Stephany Silva Dantas - 822223694
+Maria Victoria Bezerra da Silva - 8222242697
--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from app.models import ( 
+    Usuario, AreaNegocio, EquipeAtendimento, 
+    UnidadeNegocio, Contatos, Ocorrencias
+)
+
+class CustomUserAdmin(UserAdmin):
+    model = Usuario
+    fieldsets = UserAdmin.fieldsets + (
+        ('Campos Customizados', {'fields': ('area_de_negocio', 'cargo', 'gerente', 'equipe_de_atendimento')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Campos Customizados', {'fields': ('area_de_negocio', 'cargo', 'gerente', 'equipe_de_atendimento')}),
+    )
+
+# Registre seus modelos
+admin.site.register(Usuario, CustomUserAdmin)
+admin.site.register(AreaNegocio)
+admin.site.register(EquipeAtendimento)
+admin.site.register(UnidadeNegocio)
+admin.site.register(Contatos)
+admin.site.register(Ocorrencias)
