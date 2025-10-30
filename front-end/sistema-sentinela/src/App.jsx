59e790f03ee16872fd1ddb8cd5eab97af5ab66e9
Maria Eduarda Pereira <pmariaeduarda5588@outlook.com>
2025-10-30 01:10:29 -0300

    feat(sistema-sentinela): Create the React application

---
+++ front-end/sistema-sentinela/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center text-primary">Olá, Bootstrap com React e Vite! 💙</h1>
+      <button className="btn btn-success mt-3">Clique aqui</button>
+    </div>
+  )
+}
+
+export default App
