3461b4c17292351bde75286dfd3f2ea4305e34bb
Maria Eduarda <pmariaeduarda5588@outlook.com>
2025-10-31 18:06:52 -0300

    feat(Occurrences/NewOccurrenceModal): Create an occurrences page and a new occurrence modal component

--- front-end/sistema-sentinela/src/App.jsx
+++ front-end/sistema-sentinela/src/App.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
-
+import Occurrences from "./pages/Occurrences";
 
 function App() {
   return (
     <Router>
       <div className="d-flex">
         <Sidebar />
+        <div className="flex-grow-1 p-4 bg-light" style={{ minHeight: "100vh", width: "calc(100vw - 240px)" }}>
+          <Routes>
+            <Route path="/ocorrencias" element={<Occurrences />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   )
 }
 
-
-export default App
+export default App;
